d9d0dab78ffc7c60718cec3fbac4031d15c678b7
Deva16112012 <100427772+Deva16112012@users.noreply.github.com>
2022-03-23 20:41:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function c(){
+x=Number(document.getElementById("i").value); 
+z=Number(document.getElementById("t").value); 
+c=x+z;
+v=x-z;
+b=x*z;
+n=x/z;
+console.log("Addition is" ,c);
+console.log("Subraction is" ,v);
+console.log("Multiplication is" ,b)
+console.log("Divison is" ,n)
+}
